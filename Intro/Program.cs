7090777c24fc130169b1e2987c6ca7c488150bd8
Галима <@DESKTOP-PGIEF0N>
2023-07-11 18:14:09 +0300

    Add project files.

---
+++ Intro/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Intro
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.Title = "Привет дотнет";
+			Console.WriteLine("\t\tHello, World!");
+			#region CONSOLE
+			/*Console.BackgroundColor = ConsoleColor.Blue;
+			Console.CursorLeft = 22;
+			Console.CursorTop = 12;
+			Console.Write("Привет, дотнет))\n");
+
+			Console.BackgroundColor = System.ConsoleColor.Green;
+			Console.CursorLeft = 32;
+			Console.CursorTop = 5;
+			Console.Write("Привет, дотнет))\n");
+			Console.BackgroundColor = ConsoleColor.Black;
+
+			//Console.Beep(44, 2000);*/
+#endregion CONSOLE
+
+		}
+	}
+}
